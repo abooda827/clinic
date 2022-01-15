1955d0453bc4916a0728b8fab0410ae52785e7ca
abooda827 <bodeahmed827@gmail.com>
2022-01-15 16:42:07 +0200

    add files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { AboutUsComponent } from './components/about-us/about-us.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { PricingPlansComponent } from './components/pricing-plans/pricing-plans.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
+import { HowItWorksComponent } from './components/how-it-works/how-it-works.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component: HomeComponent },
+  { path: 'about' , component: AboutUsComponent },
+  { path: 'contact' , component: ContactUsComponent },
+  { path: 'pricing' , component: PricingPlansComponent },
+  { path: 'work' , component: HowItWorksComponent },
+  { path: '**' , component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { PricingPlansComponent } from './components/pricing-plans/pricing-plans.component';
+import { HowItWorksComponent } from './components/how-it-works/how-it-works.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    PricingPlansComponent,
+    HowItWorksComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
