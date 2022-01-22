c61cd02ccdf8fb1bb690d25f47c5d0611eac9c09
abooda827 <bodeahmed827@gmail.com>
2022-01-22 02:21:46 +0200

    cotact us style and clip path

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faEnvelope , faUser } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
+  faUser = faUser;
+  faEnvelope = faEnvelope;
 
   constructor() { }
 
