959ebe47a65b37679d5b061cbf7b38f968ddc4e0
abooda827 <bodeahmed827@gmail.com>
2022-01-27 15:29:02 +0200

    adding search bar and update main image size

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { faCoffee , faTooth , faHeartbeat , faPills , faCheckCircle} from '@fortawesome/free-solid-svg-icons';
+import { faCoffee , faTooth , faHeartbeat , faPills , faCheckCircle , faSearch} from '@fortawesome/free-solid-svg-icons';
 import { faApple , faAndroid } from '@fortawesome/free-brands-svg-icons';
+import { faUser , faCalendarCheck} from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,6 +12,9 @@ export class HomeComponent implements OnInit {
   faCoffee = faCoffee;
   faApple = faApple;
   faAndroid = faAndroid;
+  faSearch = faSearch;
+  faUser = faUser;
+  faCalendarCheck = faCalendarCheck;
   faTooth = faTooth;
   faHeartbeat =faHeartbeat;
   faPills = faPills;
